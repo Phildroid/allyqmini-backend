90742543062f464d7a0860be2be153a7938d4cea
Phildroid <oyedelephilemon@gmail.com>
2026-03-10 11:36:41 +0100

    More tweaks...

--- rag_engine.py
+++ rag_engine.py
@@ -14,6 +14,7 @@ from langchain_core.messages import HumanMessage
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
+from langchain_core.embeddings import Embeddings
 
 import pypdf
 from pptx import Presentation

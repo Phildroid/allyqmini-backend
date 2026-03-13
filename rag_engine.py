43debc8cdb4e647833665bef30c7c42e18ff185c
Phildroid <oyedelephilemon@gmail.com>
2026-03-13 14:28:47 +0100

    Model change

--- rag_engine.py
+++ rag_engine.py
@@ -34,7 +34,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-1.5-flash-001",
     temperature=0,
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     convert_system_message_to_human=True,
